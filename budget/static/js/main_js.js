aab7128263375dba3d0b6860b8470e4984ee7b0c
Cody Pettit (Smaug) <cpett26@gmail.com>
2016-11-05 15:56:55 -0600

    Adds sign up modal

--- budget/static/js/main_js.js
+++ budget/static/js/main_js.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
+
+  $('.btnSignUp').click(function(e){
+    e.preventDefault();
+    $.ajax({
+        url: '/sign_up',
+        success: function(data) {
+          $('#modal').find('.modal_container').html(data);
+          $('#modal').openModal();
+        }
+    });
+  });
 });
