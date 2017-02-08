8bdb99560ce5bf35793ba1f38a090ade7f099265
tanner-sawyer <tanner.sawyer@byu.edu>
2017-02-07 20:58:40 -0700

    Fix issue #9

--- homepage/static/js/goals.js
+++ homepage/static/js/goals.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     closeOnSelect: true
   });
 
-  $('#btnAddGoals').click(function(e){
+  $('.btnAddGoals').click(function(e){
     e.preventDefault();
     e.stopImmediatePropagation();
     $.ajax({
